b18e07ecf78ba217d08388ea78b39cfb2cbb1ad2
Fernando Gurgel <fag.gurgel@gmail.com>
2020-08-18 20:10:07 -0400

    Initial commit

---
+++ src/pages/SingIn/index.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import { Container } from './styles'
+
+class SingIn extends Component {
+  render() {
+    return <Container />
+  }
+}
+
+export default SingIn
+
+// const SingIn: React.FC = () => {
+//   return <Container />
+// }
---
+++ src/pages/SingUp/index.tsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+
+import { Container } from './styles'
+
+const SingUp: React.FC = () => {
+  return <Container />
+}
+export default SingUp
